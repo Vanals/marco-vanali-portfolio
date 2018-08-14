a744c5c71eedd8555d450596743b8c67da4f656f
Vanals <marco91vanali@gmail.com>
2018-08-14 16:29:08 +0100

    Animated 2 stars

--- src/components/landingPresentation/Stars.js
+++ src/components/landingPresentation/Stars.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import styles from '../../styling/css/LandingPresentation.css'
+import React, { Component } from 'react';
+import styles from '../../styling/css/Stars.css'
+import { TimelineMax } from "gsap/all";
 
-const Stars = ({questionText}) => {
-    return (
-      <svg height="15" width="15" >
-        <circle cx="3" cy="3" r="3" fill="white" className={styles.star}/>
-        <circle cx="3" cy="3" r="3" fill="white" className={styles.star}/>
-      </svg>
+class Stars extends Component {
 
-    );
+  constructor() {
+    super()
+    this.state = {
+      star1: React.createRef(),
+      star2: React.createRef()
+    }
+  }
+
+  componentDidMount() {
+    this.startStarsAnimation()
+  }
+
+  startStarsAnimation = () => {
+    const masterTl = new TimelineMax();
+    masterTl
+              .add(this.clearStage())
+              .add(this.enterStar())
+  }
+
+  clearStage = () => {
+    let clearTl= new TimelineMax();
+
+    clearTl
+            .set(this.state.star1.current, {autoAlpha: 0})
+            .set(this.state.star2.current, {autoAlpha: 0})
+            ;
+    return clearTl
+  }
+
+  enterStar = () => {
+    let starTl= new TimelineMax();
+    starTl
+
+          .fromTo(this.state.star1.current, 1, {autoAlpha:0, y:0}, {autoAlpha: 1, y:20})
+          .fromTo(this.state.star2.current, 1, {autoAlpha:0, y:0}, {autoAlpha: 1, y:20})
+    return starTl
+  }
+
+    render() {
+      return (
+        <div>
+          <svg height="12" width="12" ref={this.state.star1} id={styles.starSvg1}>
+            <circle cx="6" cy="6" r="3"  className={styles.circle} />
+          </svg>
+
+          <svg height="12" width="12" ref={this.state.star2} id={styles.starSvg2}>
+            <circle cx="6" cy="6" r="3"  className={styles.circle} />
+          </svg>
+        </div>
+      );
+    }
 }
 
 
