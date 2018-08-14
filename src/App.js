abd90cff7b672b1843bc1e723bfff1ae59e0bd33
Vanals <marco91vanali@gmail.com>
2018-08-14 11:04:51 +0100

    CSS-modules tested and working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from "./styling/css/App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className= {styles.nameTitleContainer} >
+        <h1 className= {styles.name}>Marco Vanali</h1>
+        <h2 classnbame={styles.title}>Web Developer</h2>
       </div>
     );
   }
